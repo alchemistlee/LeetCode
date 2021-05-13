a70d18abd8062f7cb0ca80b971d8b658ebb9b994
李一凡 <yifan.li@dianping.com>
2021-05-13 23:03:49 +0800

    up

---
+++ src/main/java/com/jump/leetcode/stack/MinStack.java
@@ -0,0 +1,38 @@
+package com.jump.leetcode.stack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinStack {
+
+    private List<Integer> d ;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        this.d = new ArrayList<Integer>();
+    }
+
+    public void push(int x) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int getMin() {
+    }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+}
