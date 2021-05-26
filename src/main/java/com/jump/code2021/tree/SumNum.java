dd4e548eabeff3c57d6620f8845c8e5ce13a4249
isaac lee <alchemistlee@admindeMacBook-Pro-2.local>
2021-05-26 20:03:26 +0800

    up

---
+++ src/main/java/com/jump/code2021/tree/SumNum.java
@@ -0,0 +1,28 @@
+package com.jump.code2021.tree;
+
+public class SumNum {
+
+    private static class TreeNode {
+
+        int val;
+        TreeNode left=null;
+        TreeNode right=null;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int sumNumbers(TreeNode root) {
+
+    }
+
+
+
+    public static void main(String[] args){
+
+    }
+}
