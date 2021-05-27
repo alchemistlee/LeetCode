3c54322e82126d070e6be12c57780805cf1239e5
isaac lee <alchemistlee@admindeMacBook-Pro-2.local>
2021-05-27 11:35:58 +0800

    up

--- src/main/java/com/jump/code2021/tree/UpsideDownBinTree.java
+++ src/main/java/com/jump/code2021/tree/UpsideDownBinTree.java
@@ -17,10 +17,40 @@ public class UpsideDownBinTree {
     }
 
     public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if(root==null){
+            return null;
+        }
 
+        TreeNode newRoot =upsideDownBinaryTree(root.left);
+
+        if(newRoot==null){
+            newRoot = root;
+            return newRoot;
+        }
+
+        TreeNode curRight = root.right;
+        root.left.left = curRight;
+        root.left.right= root;
+        return newRoot;
     }
 
     public static void main(String[] args){
 
+        TreeNode r1 = new TreeNode(1);
+        TreeNode n11 = new TreeNode(2);
+        TreeNode n12 = new TreeNode(3);
+        TreeNode n31 = new TreeNode(4);
+        TreeNode n32 = new TreeNode(5);
+        TreeNode n33 = new TreeNode(4);
+
+        r1.left=n11;
+        r1.right =n12;
+        n11.left=n31;
+        n11.right=n32;
+
+
+        UpsideDownBinTree u = new UpsideDownBinTree();
+        TreeNode res =  u.upsideDownBinaryTree(r1);
+        System.out.println(res);
     }
 }
