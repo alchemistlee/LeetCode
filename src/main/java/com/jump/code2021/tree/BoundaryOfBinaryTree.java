227c3945dff8d008a9ebb553fbcc908f9d7c6a0b
isaac lee <alchemistlee@admindeMacBook-Pro-2.local>
2021-06-03 21:00:45 +0800

    up

---
+++ src/main/java/com/jump/code2021/tree/BoundaryOfBinaryTree.java
@@ -0,0 +1,28 @@
+package com.jump.code2021.tree;
+
+import java.util.List;
+
+public class BoundaryOfBinaryTree {
+
+    private static class TreeNode {
+
+        int val;
+        TreeNode left=null;
+        TreeNode right=null;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
