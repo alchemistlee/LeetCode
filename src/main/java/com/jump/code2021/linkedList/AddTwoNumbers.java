578c8e1be06cc4f9bd3d97a4da2360daf54d6c14
isaac lee <alchemistlee@admindeMacBook-Pro-2.local>
2021-07-14 15:35:05 +0800

    up new cate

---
+++ src/main/java/com/jump/code2021/linkedList/AddTwoNumbers.java
@@ -0,0 +1,22 @@
+package com.jump.code2021.linkedList;
+
+public class AddTwoNumbers {
+
+    static private class ListNode {
+        int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    }
+
+    public static void main(String[] args){
+
+    }
+
+}
