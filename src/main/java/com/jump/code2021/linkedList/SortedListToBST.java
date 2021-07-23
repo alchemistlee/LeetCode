503f6f4dc0106a41fa1efe451c2c5f9ad3cc96ac
isaac lee <alchemistlee@admindeMacBook-Pro-2.local>
2021-07-23 11:57:38 +0800

    up

---
+++ src/main/java/com/jump/code2021/linkedList/SortedListToBST.java
@@ -0,0 +1,36 @@
+package com.jump.code2021.linkedList;
+
+public class SortedListToBST {
+
+    static private class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    }
+
+    private static class TreeNode {
+
+        int val;
+        TreeNode left=null;
+        TreeNode right=null;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    public TreeNode sortedListToBST(ListNode head) {
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
