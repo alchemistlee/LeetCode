201368e110900fd40e591b2afa4f1cd8a8a711c9
isaac lee <alchemistlee@admindeMacBook-Pro-2.local>
2021-08-04 19:27:24 +0800

    up

---
+++ src/main/java/com/jump/code2021/linkedList/AddTwoNumbers2.java
@@ -0,0 +1,20 @@
+package com.jump.code2021.linkedList;
+
+public class AddTwoNumbers2 {
+
+    static private class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    }
+
+    public static void  main(String[] args){
+
+    }
+}
