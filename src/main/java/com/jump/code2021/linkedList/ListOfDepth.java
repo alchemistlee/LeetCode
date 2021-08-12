591850ae153cb7dc97f4d3a0de6f19f05168fe3e
isaac lee <alchemistlee@admindeMacBook-Pro-2.local>
2021-08-12 20:53:20 +0800

    up\

---
+++ src/main/java/com/jump/code2021/linkedList/ListOfDepth.java
@@ -0,0 +1,31 @@
+package com.jump.code2021.linkedList;
+
+public class ListOfDepth {
+
+
+
+    private static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+    }
+
+
+    static private class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+
+    public ListNode[] listOfDepth(TreeNode tree) {
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
