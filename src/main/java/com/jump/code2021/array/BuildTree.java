f6e97e4126955a5be1d5b92ff94260bf9f6a3985
alchemistlee <bingyanlyf@gmail.com>
2021-10-15 17:25:39 +0800

    up

---
+++ src/main/java/com/jump/code2021/array/BuildTree.java
@@ -0,0 +1,27 @@
+package com.jump.code2021.array;
+
+public class BuildTree {
+
+
+     public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
+
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
